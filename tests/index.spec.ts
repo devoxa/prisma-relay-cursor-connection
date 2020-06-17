e4d04ad9e4a70981adb42f3ceea69163a1b4b5b5
queicherius <queicherius@gmail.com>
2020-06-17 22:37:20 +0000

    Reduce code duplication in tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { findManyCursorConnection } from '../src'
+import { findManyCursorConnection, ConnectionArguments } from '../src'
 import { TODO_FIXTURES } from './fixtures'
 
 describe('prisma-relay-cursor-connection', () => {
@@ -30,22 +30,26 @@ describe('prisma-relay-cursor-connection', () => {
     expect(result).toMatchSnapshot()
   })
 
-  it('returns all TODOs', async () => {
+  const VALID_CASES: Array<[string, ConnectionArguments]> = [
+    ['returns all TODOs', { first: 10000 }],
+    ['returns the first 5 TODOs', { first: 5 }],
+    ['returns the first 5 TODOs after the 1st todo', { first: 5, after: 'id_01' }],
+    ['returns the first 5 TODOs after the 5th todo', { first: 5, after: 'id_05' }],
+    ['returns the first 5 TODOs after the 15th todo', { first: 5, after: 'id_15' }],
+    ['returns the first 5 TODOs after the 16th todo', { first: 5, after: 'id_16' }],
+    ['returns the first 5 TODOs after the 20th todo', { first: 5, after: 'id_20' }],
+    ['returns the last 5 TODOs', { last: 5 }],
+    ['returns the last 5 TODOs before the 1st todo', { last: 5, before: 'id_01' }],
+    ['returns the last 5 TODOs before the 5th todo', { last: 5, before: 'id_05' }],
+    ['returns the last 5 TODOs before the 6th todo', { last: 5, before: 'id_06' }],
+    ['returns the last 5 TODOs before the 16th todo', { last: 5, before: 'id_16' }],
+  ]
+
+  test.each(VALID_CASES)('%s', async (name, connectionArgs) => {
     const result = await findManyCursorConnection(
       (args) => client.todo.findMany(args),
       () => client.todo.count(),
-      { first: 20 }
-    )
-
-    expect(result.pageInfo).toMatchSnapshot()
-    expect(result.edges.map((edge) => edge.node)).toEqual(TODO_FIXTURES)
-  })
-
-  it('returns the first 5 TODOs', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { first: 5 }
+      connectionArgs
     )
 
     expect(result).toMatchSnapshot()
@@ -75,269 +79,40 @@ describe('prisma-relay-cursor-connection', () => {
     result.edges[0].node.text
   })
 
-  it('returns the first 5 TODOs after the 5th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { first: 5, after: 'id_05' }
-    )
+  const INVALID_CASES: Array<[string, ConnectionArguments]> = [
+    ['errors for invalid arguments (negative first)', { first: -5 }],
+    ['errors for invalid arguments (negative last)', { last: -5 }],
+    ['errors for invalid arguments (no first & no last)', {}],
+    ['errors for invalid arguments (both first & last)', { first: 5, last: 5 }],
+    ['errors for invalid arguments (both after & before)', { after: 'id_05', before: 'id_15' }],
+    [
+      'errors for invalid arguments (both after & before with first)',
+      { first: 5, after: 'id_05', before: 'id_15' },
+    ],
+    ['errors for invalid arguments (after without first)', { after: 'id_05' }],
+    ['errors for invalid arguments (before without last)', { before: 'id_15' }],
+    ['errors for invalid arguments (after with last)', { last: 5, after: 'id_05' }],
+    ['errors for invalid arguments (before with first)', { first: 5, before: 'id_15' }],
+    [
+      'errors for invalid arguments (kitchensink)',
+      { first: 5, after: 'id_05', last: 5, before: 'id_15' },
+    ],
+  ]
 
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the first 5 TODOs after the 15th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { first: 5, after: 'id_15' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the first 5 TODOs after the 16th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { first: 5, after: 'id_16' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the first 5 TODOs after the 20th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { first: 5, after: 'id_20' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the last 5 TODOs', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { last: 5 }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the last 5 TODOs before the 5th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { last: 5, before: 'id_05' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the last 5 TODOs before the 6th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { last: 5, before: 'id_06' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the last 5 TODOs before the 16th todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { last: 5, before: 'id_16' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('returns the last 5 TODOs before the 1st todo', async () => {
-    const result = await findManyCursorConnection(
-      (args) => client.todo.findMany(args),
-      () => client.todo.count(),
-      { last: 5, before: 'id_01' }
-    )
-
-    expect(result).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (negative first)', async () => {
+  test.each(INVALID_CASES)('%s', async (name, connectionArgs) => {
     let error
 
     try {
       await findManyCursorConnection(
         (args) => client.todo.findMany(args),
         () => client.todo.count(),
-        { first: -5 }
+        connectionArgs
       )
     } catch (err) {
       error = err
     }
 
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (negative last)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { last: -5 }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (no first & no last)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        {}
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (both first & last)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { first: 5, last: 5 }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (both after & before)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { after: 'id_05', before: 'id_15' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (both after & before with first)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { first: 5, after: 'id_05', before: 'id_15' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (after without first)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { after: 'id_05' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (before without last)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { before: 'id_15' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (after with last)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { last: 5, after: 'id_05' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (before with first)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { first: 5, before: 'id_15' }
-      )
-    } catch (err) {
-      error = err
-    }
-
-    expect(error).toMatchSnapshot()
-  })
-
-  it('errors for invalid arguments (kitchensink)', async () => {
-    let error
-
-    try {
-      await findManyCursorConnection(
-        (args) => client.todo.findMany(args),
-        () => client.todo.count(),
-        { first: 5, after: 'id_05', last: 5, before: 'id_15' }
-      )
-    } catch (err) {
-      error = err
-    }
-
+    expect(error).not.toEqual(undefined)
     expect(error).toMatchSnapshot()
   })
 })
